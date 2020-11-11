1cbb752cc195ac9fb2af4abd064682afc454511c
tatsu98s <73688529+tatsu98s@users.noreply.github.com>
2020-11-11 18:08:19 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,6 @@
 class RoomsController < ApplicationController
-  
-  def index
-  end
+  # def index
+  # end
   
   def new
     @room = Room.new
@@ -17,13 +16,14 @@ class RoomsController < ApplicationController
   end
 
   def destroy
-    room = room.find(params[:id])
+    room = Room.find(params[:id])
     room.destroy
     redirect_to root_path
+  end
 
   private
 
   def room_params
-    params.require(:room).permit(:name, user_ids: [])
+    params.require(:room).permit(:name, user_ids:[ ])
   end
 end
